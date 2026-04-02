c59b4f37f46f4f4803d5e0397a420f5d92f93713
Solomon Chan Kay Pong <95954478+PannaCottaSolomon@users.noreply.github.com>
2026-04-02 17:14:54 +0800

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        res = []
+
+        for i, num in enumerate(nums):
+            remainder = target - num
+            if remainder in nums[i + 1:]:
+                idx1 = i
+                res.append(idx1)
+
+                nums2 = nums[i + 1:]
+                print(nums2)
+                idx2 = nums2.index(remainder)
+                idx2 += i + 1
+                res.append(idx2)
+                break
+
+        return res
