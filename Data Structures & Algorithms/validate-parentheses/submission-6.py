4d27c474107725b32672b98a2db88847ff22d1d9
SolomonChan <95954478+PannaCottaSolomon@users.noreply.github.com>
2026-08-19 22:31:58 +0800

    Add: validate-parentheses - submission-6

---
+++ Data Structures & Algorithms/validate-parentheses/submission-6.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) < 2:
+            return False
+
+        stk = []
+        left = ["(", "{", "["]
+        complete = ["()", "{}", "[]"]
+
+        for i, curr in enumerate(s):
+            if curr in left:
+                stk.append(curr)
+            elif len(stk) > 0:
+                bracket = stk.pop()
+                bracket += curr
+                if bracket not in complete:
+                    return False
+            else:
+                return False
+
+        return True
