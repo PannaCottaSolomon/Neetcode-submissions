50a2a023e3d9c1091b8502c9ba9af986b8082625
Solomon <95954478+PannaCottaSolomon@users.noreply.github.com>
2026-09-03 18:06:17 +0800

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        if len(piles) == h:
+            return max(piles)
+        elif len(piles) == 1:
+            return piles[0]
+
+        piles.sort()
+        # print(piles)
+        ans = max(piles)
+        for height in range(ans, 0, -1):
+            # print(height)
+            counts = []
+            for num in piles:
+                if num <= height:
+                    counts.append(1)
+                else:
+                    repeats = (num // height) + 1 if num % height > 0 else num // height
+                    counts.append(repeats)
+            
+            # print(counts)
+            total = sum(counts)
+            if total <= h:
+                ans = height
+            else:
+                break
+
+        return ans
